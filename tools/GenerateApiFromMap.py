2e604fa7fc04910186e466aa8d33609416da6e35
latufla <lafufla.work@gmail.com>
2017-10-13 06:30:16 +0700

    rtor generate api from map

--- tools/GenerateApiFromMap.py
+++ tools/GenerateApiFromMap.py
@@ -6,37 +6,37 @@ from tools.ApiMap import api_map
 with open('tools/ApiMap.json', 'r') as myFile:
     data = myFile.read().replace('\n', '')
 
-for a in api_map:
-    keys = a.keys()
-    for k in keys:
-        if k == "!name" \
-                or k == "require" \
-                or k == "requireOnce" \
-                or k == "gs_load":
+for packages in api_map:
+    packageNames = packages.keys()
+    for pName in packageNames:
+        if pName == "!name" \
+                or pName == "require" \
+                or pName == "requireOnce" \
+                or pName == "gs_load":
             continue
 
-        f = open("api_generated/" + k + ".js", "w+")
-        f.write("function " + k + " (type) {}" + "\n\n")
-        for v in a[k]:
-            if v == "!doc":
+        f = open("api_generated/" + pName + ".js", "w+")
+        f.write("function " + pName + " (type) {}" + "\n\n")
+        for fName in packages[pName]:
+            if fName == "!doc":
                 continue
 
-            info = a[k][v]
-            infoStr = "/** \n"
-            if "!type" in info:
-                type = info["!type"]
-                type = type.replace("<br/>", "<p>")
-                infoStr += " * " + type + "\n * <p> \n"
+            func = packages[pName][fName]
+            jsdoc = "/** \n"
+            if "!type" in func:
+                signature = func["!type"]
+                signature = signature.replace("<br/>", "<p>")
+                jsdoc += " * " + signature + "\n * <p> \n"
 
-            if "!doc" in info:
-                doc = info["!doc"]
-                doc = doc.replace("<br/>", "<p>")
-                doc = doc.split("example", 1)[0]
-                infoStr += " * " + doc + "\n * \n"
+            if "!doc" in func:
+                description = func["!doc"]
+                description = description.replace("<br/>", "<p>")
+                description = description.split("example", 1)[0]
+                jsdoc += " * " + description + "\n * \n"
 
-            infoStr += "*/ \n"
-            f.write(infoStr)
+            jsdoc += "*/ \n"
+            f.write(jsdoc)
 
-            f.write(k + "." + v + " = function(){};" + "\n\n\n")
+            f.write(pName + "." + fName + " = function(){};" + "\n\n\n")
 
         f.close()
